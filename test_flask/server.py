0d88661dc463ae2c631eb915400cab713113b303
MrLYC <imyikong@gmail.com>
2015-09-01 16:44:24 +0800

    add calc

--- test_flask/server.py
+++ test_flask/server.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask
+import math
+
+from flask import Flask, request
 from flask import render_template
 
+from ycyc.base.calctools import safecalc
+from ycyc.base.funcutils import iter_attrs
+
 app = Flask(__name__)
+
+Calc_Func = {
+    n: f
+    for n, f in iter_attrs(math, exclude_methods=False)
+}
 
 
 @app.route("/", methods=["GET"])
@@ -12,6 +22,17 @@ def index():
     return render_template("index.html", init_lines=(
         u"Welcome",
     ))
+
+
+@app.route("/calc", methods=["POST"])
+def calc():
+    expr = request.form.get("expr")
+    if not expr:
+        return ""
+    try:
+        return str(safecalc(expr, Calc_Func))
+    except Exception as err:
+        return "Error: %s" % err
 
 
 def main():
